e50370c2d3a704e211eed9b23d9f04779ec0fbf6
andyc <andycolo78@gmail.com>
2025-02-25 22:45:29 +0100

    Fix progress bar on new line

--- App/helpers/progress_bar.py
+++ App/helpers/progress_bar.py
@@ -1,9 +1,12 @@
 import sys
 
+
 class ProgressBar:
 
     @staticmethod
-    def print(progress, total, length=50):
+    def print(progress, total, length=50, last:bool=False):
         bar = "█" * int(length * progress / total) + "-" * (length - int(length * progress / total))
         sys.stdout.write(f"\r|{bar}| {progress}/{total}")
+        if progress==total:
+            sys.stdout.write("\n")
         sys.stdout.flush()
